49cdbbbf3045ee20a7a343f8112442678be28d48
Kana <kana@ksharp.net>
2026-06-20 02:00:13 +0100

    Move Veni in BotGateway folder

---
+++ FFXIVVenues.BotGateway/Utils/ComponentBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using System;
+using System.Threading.Tasks;
+using FFXIVVenues.Veni.Infrastructure.Components;
+using FFXIVVenues.Veni.Infrastructure.Context;
+using FFXIVVenues.Veni.Infrastructure.Context.SessionHandling;
+using FFXIVVenues.Veni.VenueControl;
+
+namespace FFXIVVenues.Veni.Utils
+{
+    internal static class ComponentBuilderExtensions
+    {
+
+        public static ComponentBuilder WithSkipButton<SkipTarget, ModifyTarget>(this ComponentBuilder builder, IVeniInteractionContext context) 
+            where SkipTarget : ISessionState 
+            where ModifyTarget : ISessionState
+        {
+            return builder.WithButton("►  Next/Skip", context.Session.RegisterComponentHandler(c =>
+            {
+                if (c.Session.InEditing())
+                    return c.Session.MoveStateAsync<ModifyTarget>(c);
+                return c.Session.MoveStateAsync<SkipTarget>(c);
+            }, ComponentPersistence.ClearRow), ButtonStyle.Secondary);
+        }
+
+        public static ComponentBuilder WithBackButton(this ComponentBuilder builder, IVeniInteractionContext context, Func<Task<bool>> @override = null)
+        {   
+            return builder.WithButton("◄  Back", context.Session.RegisterComponentHandler(async c =>
+            {
+                var result = false;
+                if (@override != null) result = await @override();
+                else result = await c.Session.TryBackStateAsync(c);
+                if (result) _ = c.Interaction.ModifyOriginalResponseAsync(props => props.Components = new ComponentBuilder().Build());
+                else await c.Interaction.Channel.SendMessageAsync(embed: new EmbedBuilder().WithDescription("Cannot not go back any more!").Build());
+            }, ComponentPersistence.ClearRow), ButtonStyle.Secondary);
+        }
+
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-Veni (or Veni Ki) is a Discord AI with language understanding and modern slash commands for creating and managing venues with FFXIV Venues. She has full lore for her creation and growth as an Indexer within the team. You are able to create new venue, edits existing venues, delete them 🥲, open and close them. 
-
-
-![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/FFXIVVenues/ffxiv-venues-veni/main_veni-release.yml)
